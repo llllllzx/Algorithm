29853af59f7f8f99c9de9e3381631712083f5a78
llllllzx <llllllllll_zx@163.com>
2019-03-22 14:06:51 +0800

    '小和问题'

--- src/Sort/Code_06_SmallSum.java
+++ src/Sort/Code_06_SmallSum.java
@@ -2,47 +2,58 @@ package Sort;
 
 import java.util.Arrays;
 
-public class Code_05_MergeSort {
+public class Code_06_SmallSum {
 
-	public static void mergeSort(int[] arr) {
-		if (arr == null || arr.length < 2) {
-			return;
+	public static int smallSum(int[] arr) {
+		if(arr == null || arr.length < 2){
+			return 0;
 		}
-		mergeSort(arr, 0, arr.length - 1);
+		return mergeSort(arr,0,arr.length-1);
 	}
 
-	public static void mergeSort(int[] arr, int l, int r) {
-		if (l == r) {
-			return;
+	public static int mergeSort(int[] arr, int l, int r) {
+		if(l == r){
+			return 0;
 		}
-		int mid = l + ((r - l) >> 1);
-		mergeSort(arr, l, mid);
-		mergeSort(arr, mid + 1, r);
-		merge(arr, l, mid, r);
+			int middle = l + ((r - l) >> 1);
+		return  mergeSort(arr, l, middle) + mergeSort(arr, middle + 1, r) + merge(arr, l, middle, r);
 	}
 
-	public static void merge(int[] arr, int l, int m, int r) {
-		int[] help = new int[r - l + 1];
-		int i = 0;
+	public static int merge(int[] arr, int l, int m, int r) {
+		int[] help = new int[r - l +1];
 		int p1 = l;
 		int p2 = m + 1;
-		while (p1 <= m && p2 <= r) {
-			help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+		int i = 0;
+		int res = 0;
+		while(p1 <= m && p2 <= r){
+			res += arr[p1] < arr[p2] ? (r-p2 + 1) * arr[p1] : 0;
+			help[i++] = arr[p1] < arr[p2] ? arr[p1++]:arr[p2++];
 		}
-		while (p1 <= m) {
+
+		while(p1 <= m){
 			help[i++] = arr[p1++];
 		}
-		while (p2 <= r) {
-			help[i++] = arr[p2++];
+		while(p2 <= r){
+			help[i++] = arr[p2 ++ ];
 		}
-		for (i = 0; i < help.length; i++) {
+		for(i = 0;i<help.length;i++){
 			arr[l + i] = help[i];
 		}
+		return res;
 	}
 
 	// for test
-	public static void comparator(int[] arr) {
-		Arrays.sort(arr);
+	public static int comparator(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return 0;
+		}
+		int res = 0;
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = 0; j < i; j++) {
+				res += arr[j] < arr[i] ? arr[j] : 0;
+			}
+		}
+		return res;
 	}
 
 	// for test
@@ -105,9 +116,7 @@ public class Code_05_MergeSort {
 		for (int i = 0; i < testTime; i++) {
 			int[] arr1 = generateRandomArray(maxSize, maxValue);
 			int[] arr2 = copyArray(arr1);
-			mergeSort(arr1);
-			comparator(arr2);
-			if (!isEqual(arr1, arr2)) {
+			if (smallSum(arr1) != comparator(arr2)) {
 				succeed = false;
 				printArray(arr1);
 				printArray(arr2);
@@ -115,12 +124,4 @@ public class Code_05_MergeSort {
 			}
 		}
 		System.out.println(succeed ? "Nice!" : "Fucking fucked!");
-
-		int[] arr = generateRandomArray(maxSize, maxValue);
-		printArray(arr);
-		mergeSort(arr);
-		printArray(arr);
-
-	}
-
-}
+	}}
